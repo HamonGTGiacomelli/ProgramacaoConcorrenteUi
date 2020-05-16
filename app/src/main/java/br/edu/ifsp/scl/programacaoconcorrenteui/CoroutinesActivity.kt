f7f0c7f0ddbe15a12b3f024705a35ff18bd871bf
hamon <hamongtgiacomelli@gmail.com>
2020-05-16 16:26:01 -0300

    change to CoroutineScope

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -3,18 +3,18 @@ package br.edu.ifsp.scl.programacaoconcorrenteui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
 
-
     override fun run() {
-        GlobalScope.launch {
-            runOnUiThread {
-                altaIv.setImageResource(R.drawable.android_preto)
-                baixaIv.setImageResource(R.drawable.android_verde)
-            }
+
+        CoroutineScope(Dispatchers.Main).launch {
+
+            delay(Constantes.CARREGA_IMAGENS_DELAY);
+
+            altaIv.setImageResource(R.drawable.android_preto)
+            baixaIv.setImageResource(R.drawable.android_verde)
         }
     }
 
